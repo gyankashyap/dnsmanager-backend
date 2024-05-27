c6cdd3aedd7c06971e08a3181b29d88e5d2a4dfd
gyankashyap <gyankashyap56@gmail.com>
2024-05-27 23:14:22 +0530

    final code for server side

--- server.js
+++ server.js
@@ -1,37 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch(err => {
-//   console.error('Error connecting to MongoDB:', err);
-// });
-
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// // Routes
-// const authRoutes = require('./routes/authRoutes');
-// const dnsRoutes = require('./routes/dnsRoutes');
-
-// app.use('/auth', authRoutes);
-// app.use('/dns', dnsRoutes);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
